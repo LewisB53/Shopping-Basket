2710bff8003a989442d8a3bb0212a5abfe899562
Lewis <lewisburns630@googlemail.com>
2017-06-09 20:50:46 +0100

    basic spec file created

---
+++ app/src/test/java/com/codeclan/user/shoppingbasket/ShoppingBasketTest.java
@@ -0,0 +1,20 @@
+package com.codeclan.user.shoppingbasket;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * Created by user on 09/06/2017.
+ */
+
+public class ShoppingBasketTest {
+
+    ShoppingBasket shoppingBasket;
+
+    @Before
+    public void before() {
+        shoppingBasket = new ShoppingBasket();
+    }
+
+}
